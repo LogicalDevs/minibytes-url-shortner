53d2dc14e4a791bb4e156b5673dbc98e5ddbba40
“SergioMOCruz” <cruzmosergio@gmail.com>
2022-02-01 20:09:48 +0000

    fix: add create user route and deleted unnecessary files

--- api-minibytes/src/routes/index.ts
+++ api-minibytes/src/routes/index.ts
@@ -2,11 +2,13 @@ import express from 'express';
 import urlRoutes from './url.routes';
 import userRoutes from './user.routes';
 import authsController from '../controllers/auths.controller';
+import usersController from '../controllers/users.controller';
 import { auth } from '../middlewares/auth';
 
 const router = express.Router();
 
 router.post("/auth/signin", authsController.login)
+router.post("/auth/signup", usersController.createUser)
 
 //Secured routes! 
 router.use(auth)
