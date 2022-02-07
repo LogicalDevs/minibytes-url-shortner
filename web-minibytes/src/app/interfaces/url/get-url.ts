e6cf440da229d26f54e91a8bcc1c1a39c600cabf
flav1o <flaviodanielmartinscosta@gmail.com>
2022-02-07 22:42:39 +0000

    chore: add url table data from api

---
+++ web-minibytes/src/app/interfaces/url/get-url.ts
@@ -0,0 +1,10 @@
+export interface GetUrl {
+    id_url: number,
+    id_user: number,
+    name_url: string,
+    created_in: number,
+    main_url: string,
+    short_url: string,
+    tags: string,
+    qr_code: string
+}
---
+++ web-minibytes/src/app/services/urls/urls.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { GetUrl } from 'src/app/interfaces/url/get-url';
+import { config } from 'src/app/config';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UrlsService {
+
+  constructor(private _http: HttpClient) { }
+
+  getUrlList(): Observable<GetUrl[]> {
+    return this._http.get<GetUrl[]>(`http://${config.apiUrl}/urls`);
+  }
+}
