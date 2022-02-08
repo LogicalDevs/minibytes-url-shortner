d8d20d26b4af9fd42fab3aac60fbfb104a260103
“SergioMOCruz” <cruzmosergio@gmail.com>
2022-02-08 15:49:52 +0000

    chore: verify uniqueness of a short URL

--- api-minibytes/src/controllers/urls.controller.ts
+++ api-minibytes/src/controllers/urls.controller.ts
@@ -35,7 +35,7 @@ export const getUrlByID = async (req: Request, res: Response): Promise<Response>
 
 export const createUrl = async (req: Request, res: Response): Promise<Response> => { 
     try{
-        let { id_user, name_url, main_url, tags } = req.body;
+        let { id_user, name_url, main_url, short_url, tags } = req.body;
 
         if(!id_user) return res.status(400).json("User ID must be inserted!")
         if(!Number(id_user)) return res.status(400).json("User ID must be a number!")
@@ -46,15 +46,17 @@ export const createUrl = async (req: Request, res: Response): Promise<Response> 
         if(!name_url) return res.status(400).json("URL Name must be inserted!")
         if(!main_url) return res.status(400).json("Main URL must be inserted!")
         if(!verifyURL.urlValidator.isValid(main_url)) return res.status(400).json("Main URL is not a valid URL!")
+        if(!short_url) return res.status(400).json("Short URL must be inserted!")
+
+        const verifyShortUrl : QueryResult = await pool.query(`SELECT count(*) FROM urls WHERE short_url = '${short_url}'`);
+        if(verifyShortUrl.rows[0] != 0) return res.status(400).json("Short URL must be unique!")
+
         if(!tags) return res.status(400).json("Tags must be inserted!")
         if(!verifyTag.tagValidator.isValid(tags)) return res.status(400).json("Tag is not a valid type!")
 
         let url: urlInterface;
         let created_in = Date.now();
         let qr_code = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + main_url;
-
-        let generatedString = CryptoJS.AES.encrypt(Date.now().toString(), '123').toString().substring(0, 8);
-        let short_url =  generatedString;
         
         url = {
             id_user,
@@ -69,7 +71,7 @@ export const createUrl = async (req: Request, res: Response): Promise<Response> 
         const response : QueryResult = await pool.query(`INSERT INTO urls (id_user, name_url, created_in, main_url, short_url, tags, qr_code) VALUES ($1, $2, $3, $4, $5, $6, $7)`, [url.id_user, url.name_url, url.created_in, url.main_url, url.short_url, url.tags, url.qr_code]);
         
         return res.json({
-            message: 'URL Created Sucessfully', 
+            message: 'URL Created Successfully', 
             body: url
         })
     }
@@ -114,7 +116,7 @@ export const getUrlByShortURL = async (req: Request, res: Response): Promise<Res
 
     try {
         const shortURL: string = req.params.shortURL;
-        const response: QueryResult = await pool.query(`SELECT * FROM urls WHERE short_url LIKE 'flavio'`);
+        const response: QueryResult = await pool.query(`SELECT * FROM urls WHERE short_url = '${shortURL}'`);
 
         return res.json(response.rows);
     }
