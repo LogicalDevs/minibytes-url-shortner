65fd7be0cec6ebc02f472cbc6f40cdadac835bec
flav1o <flaviodanielmartinscosta@gmail.com>
2022-02-08 00:45:37 +0000

    chore: add search by short url route

--- api-minibytes/src/routes/url.routes.ts
+++ api-minibytes/src/routes/url.routes.ts
@@ -5,6 +5,7 @@ import urlController from '../controllers/urls.controller';
 
 router.get('/urls', urlController.getUrls);
 router.get('/urls/:id', urlController.getUrlByID);
+router.get('/urls/byShortUrl/:shortURL', urlController.getUrlByShortURL);
 router.post('/urls', urlController.createUrl);
 router.delete('/urls/:id', urlController.deleteUrlByID);
 router.put('/urls/:id', urlController.updateUrlByID);
